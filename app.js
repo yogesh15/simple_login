311e64d3d4a3df04fe7cec918dfe09b53a612e58
yogesh <panjwaniyogesh95@gmail.com>
2019-03-12 21:25:33 +0530

    add home route

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = requie("mongoose")
+    mongoose        = require("mongoose")
 
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+        res.render("home");
+});
+
+app.listen(3000,function(){
+    console.log("The app is listening in port 3000");
+}) ;   
