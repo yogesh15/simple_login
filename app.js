7e2942a7263d90f688e13cd75fbf073063d8d19c
sushma dewangan <sush22dew@gmail.com>
2019-03-14 01:16:51 +0530

    connect to mongodb

--- app.js
+++ app.js
@@ -3,28 +3,49 @@ var express         = require("express"),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose")
 
-var registered = [] 
+mongoose.connect("mongodb://localhost:27017/simple_login",{useNewUrlParser: true});
+
+//Defining Schema for aur db
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+//making model
+var User = mongoose.model("User",userSchema);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+
+
 app.get("/",function(req,res){
         res.render("home");
 });
 
-// Taking to Register page when register button is clicked
 app.get("/registerHome",function(req,res){
     res.render("register");
 });
 
-app.get("/register",function(req,res){
-    res.render("registered",{registered: registered});
-});
+//app.get("/register",function(req,res){
+//    res.render("registered",{registered: registered});
+//});
 app.post("/register",function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-    var newRegister = {username: username, password: password} ;
-    registered.push(newRegister);
-    res.redirect("/register");
-
+    var UserData = new User({
+        username: username,
+        password: password
+    });
+    UserData.save(function(err,user){
+        if(err){
+            console.log("Something went Wrong !!!");
+            console.log(err);
+        }
+        else {
+            console.log("User added");
+            console.log(user);
+        }
+    });
+    res.render("home");
 });
 app.get("/login",function(req,res){
    // res.render("registered",{registered: registered});
